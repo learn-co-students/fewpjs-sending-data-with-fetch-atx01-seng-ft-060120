2c201decc4fcd36ae4b4f5793bb739b4108aac6e
github username <github email address>
2020-07-18 19:11:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+// const body = document.getElementById("main-body")
+// console.log(body)
+// const userForm = document.createElement("form")
+// console.log(userForm)
+// userForm.setAttribute("action", "http://localhost:3000/users")
+// userForm.setAttribute("method", "POST")
+// userForm.innerHTML= `<label> Name: <input type="text" name="userName" id="userName" /></label><br />
+// <label> Email: <input type="text" name="userEmail" id="userEmail" /></label><br />
+// <input type="submit" name="submit" id="submit" value="Submit" />`
+
+// body.appendChild(userForm)
+
+
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+
