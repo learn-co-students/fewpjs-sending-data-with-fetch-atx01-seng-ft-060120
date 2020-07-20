a544b59d3a008419c2c70003c3308c6169686cbd
bradc1993 <bradc1993@gmail.com>
2020-07-20 07:06:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+//fetch(destinationURL, configurationObject);
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+// }
+
+
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+        document.body.innerHTML = response.id
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
